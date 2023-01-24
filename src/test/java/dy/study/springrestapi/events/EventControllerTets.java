26c0d0908b79018975cd07f998eee858f1eb8dc0
dbendus0713 <dbendus0713@gmail.com>
2023-01-24 19:44:47 +0900

    spring rest docs req,res field

--- src/test/java/dy/study/springrestapi/events/EventControllerTets.java
+++ src/test/java/dy/study/springrestapi/events/EventControllerTets.java
@@ -24,7 +24,12 @@ import java.time.LocalDateTime;
 import java.util.regex.Matcher;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -84,7 +89,45 @@ public class EventControllerTets {
         .andExpect(jsonPath("_links.self").exists())
         .andExpect(jsonPath("_links.query-events").exists())
         .andExpect(jsonPath("_links.update-event").exists())
-        .andDo(document("create-event"))
+        .andDo(document("create-event",
+            links(
+                linkWithRel("self").description("link to self"),
+                linkWithRel("query-events").description("link to query events"),
+                linkWithRel("update-event").description("link to update an existing")
+            ),
+            requestHeaders(
+                headerWithName(HttpHeaders.ACCEPT).description("accept header"),
+                headerWithName(HttpHeaders.CONTENT_TYPE).description("content type")
+            ),
+            requestFields(
+                fieldWithPath("name").description("Name of new event"),
+                fieldWithPath("description").description("description of new event"),
+                fieldWithPath("beginEnrollmentDateTime").description("date time of begin of new event"),
+                fieldWithPath("closeEnrollmentDateTime").description("date time of close of new event"),
+                fieldWithPath("beginEventDateTime").description("date time of begin of event"),
+                fieldWithPath("endEventDateTime").description("date time of end of event"),
+                fieldWithPath("basePrice").description("basePrice"),
+                fieldWithPath("maxPrice").description("maxPrice"),
+                fieldWithPath("limitOfEnrollment").description("limitOfEnrollment"),
+                fieldWithPath("location").description("location")
+            ),
+            // relaxed는 정확한 문서를 만들 수 없다.
+            // relaxed는 문서 일부분만 테스트 가능하다.
+            relaxedResponseFields(
+                fieldWithPath("name").description("Name of new event"),
+                fieldWithPath("description").description("description of new event"),
+                fieldWithPath("beginEnrollmentDateTime").description("date time of begin of new event"),
+                fieldWithPath("closeEnrollmentDateTime").description("date time of close of new event"),
+                fieldWithPath("beginEventDateTime").description("date time of begin of event"),
+                fieldWithPath("endEventDateTime").description("date time of end of event"),
+                fieldWithPath("basePrice").description("basePrice"),
+                fieldWithPath("maxPrice").description("maxPrice"),
+                fieldWithPath("limitOfEnrollment").description("limitOfEnrollment"),
+                fieldWithPath("location").description("location"),
+                fieldWithPath("free").description("it tells if this event is free or not"),
+                fieldWithPath("offline").description("it tells if event is offline or not"),
+                fieldWithPath("eventStatus").description("event status"))
+        ))
     ;
   }
 
